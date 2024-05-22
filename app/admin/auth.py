2b06d8809e6e948d8f56d52fc9b37d42c57bd68d
StepanShurmin <shurmin.sa@gmail.com>
2024-05-22 13:07:37 +0400

    Доработал авторизацию, аутентификацию

--- app/admin/auth.py
+++ app/admin/auth.py
@@ -1,12 +1,12 @@
-from fastapi import Depends
-from sqladmin import Admin
+from typing import Optional
+
 from sqladmin.authentication import AuthenticationBackend
 from starlette.requests import Request
 from starlette.responses import RedirectResponse
 
-from app.exceptions import IncorrectEmailOrPasswordException
+from app.logger import logger
 from app.users.auth import authenticate_user, create_access_token
-from app.users.dependencies import get_current_user, get_token
+from app.users.dependencies import get_current_user
 
 
 class AdminAuth(AuthenticationBackend):
@@ -16,28 +16,24 @@ class AdminAuth(AuthenticationBackend):
         user = await authenticate_user(email, password)
         if user:
             access_token = create_access_token({"sub": str(user.id)})
-
-            # Validate username/password credentials
-            # And update session
             request.session.update({"token": access_token})
 
         return True
 
     async def logout(self, request: Request) -> bool:
-        # Usually you'd want to just clear the session
         request.session.clear()
         return True
 
-    async def authenticate(self, request: Request) -> bool:
+    async def authenticate(self, request: Request) -> Optional[RedirectResponse]:
         token = request.session.get("token")
+
         if not token:
             return RedirectResponse(request.url_for("admin:login"), status_code=302)
+
         user = await get_current_user(token)
+        logger.debug(f"{user=}")
         if not user:
             return RedirectResponse(request.url_for("admin:login"), status_code=302)
-
-        # Check the token in depth
-        return True
 
 
 authentication_backend = AdminAuth(secret_key="...")
